9d5ebecc73f6dc48c0cdf367eae881800d5e5393
Geuni <dlrmsgnl620@naver.com>
2024-01-03 22:21:14 +0900

    Feat: upload profile

--- app/account/account-form.tsx
+++ app/account/account-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {useCallback, useEffect, useState} from "react";
+import Avatar from "@/app/account/avatar";
 import {Database} from "@/types/supabase";
 import {
   Session,
@@ -17,6 +18,7 @@ export default function AccountForm({session}: {session: Session | null}) {
   const user = session?.user;
 
   const getProfile = useCallback(async () => {
+    if (!user?.id) return;
     try {
       setLoading(true);
 
@@ -78,58 +80,72 @@ export default function AccountForm({session}: {session: Session | null}) {
   }
 
   return (
-    <div className="form-widget">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session?.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="fullName">Full Name</label>
-        <input
-          id="fullName"
-          type="text"
-          value={fullname || ""}
-          onChange={(e) => setFullname(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="url"
-          value={website || ""}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
-      </div>
+    <>
+      <div className="form-widget">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" value={session?.user.email} disabled />
+        </div>
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            id="fullName"
+            type="text"
+            value={fullname || ""}
+            onChange={(e) => setFullname(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username || ""}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="website">Website</label>
+          <input
+            id="website"
+            type="url"
+            value={website || ""}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
 
-      <div>
-        <button
-          className="button primary block"
-          onClick={() =>
-            updateProfile({fullname, username, website, avatar_url})
-          }
-          disabled={loading}
-        >
-          {loading ? "Loading ..." : "Update"}
-        </button>
-      </div>
-
-      <div>
-        <form action="/auth/signout" method="post">
-          <button className="button block" type="submit">
-            Sign out
+        <div>
+          <button
+            className="button primary block"
+            onClick={() =>
+              updateProfile({fullname, username, website, avatar_url})
+            }
+            disabled={loading}
+          >
+            {loading ? "Loading ..." : "Update"}
           </button>
-        </form>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <Avatar
+            uid={user?.id || ""}
+            url={avatar_url}
+            size={150}
+            onUpload={(url) => {
+              setAvatarUrl(url);
+              updateProfile({fullname, username, website, avatar_url: url});
+            }}
+          />
+        </div>
+
+        <div>
+          <form action="/auth/signout" method="post">
+            <button className="button block" type="submit">
+              Sign out
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
